e26297aafc8dd6a1c5c14e31c1f5f665fc4b8c1d
Shaform <shaform@gmail.com>
2013-03-03 08:36:43 +0800

    Organize imports

--- src/net/coprg/coprg/OAuth.java
+++ src/net/coprg/coprg/OAuth.java
@@ -1,5 +1,10 @@
 package net.coprg.coprg;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.CredentialStore;
 import com.google.api.client.extensions.java6.auth.oauth2.FileCredentialStore;
@@ -10,11 +15,6 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 class OAuth {
 
